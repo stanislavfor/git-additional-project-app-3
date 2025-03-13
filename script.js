e5064ca92272f64070cbadffba229350ea328328
stanislavfor <stasposter@narod.ru>
2025-03-14 00:08:11 +0300

    fix for function getCharFrequency

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const charCountButton = document.getElementById("charCountButton");
     const resultParagraph = document.getElementById("resultParagraph");
     const charsStatsContainer = document.getElementById("charsStatsContainer");
+    const charsStatsList = document.getElementById("charsStatsList");
 
 
     // Обработчик события клика на кнопку "Очистить"
@@ -12,10 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
         inputText.value = "";
         resultParagraph.textContent = "";
         charsStatsContainer.style.display = "none";
-        statsContainer.style.display = "none";
-                while (statsList.firstChild) {
-                    statsList.removeChild(statsList.firstChild);
-                }
         while (charsStatsList.firstChild) {
             charsStatsList.removeChild(charsStatsList.firstChild);
         }
@@ -37,45 +34,45 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
     // Функция для подсчёта количества символов
-        function getCharFrequency(text) {
-            let chars = text.replace(/\s/g, "").split("");
-            let frequencyMap = new Map();
-            for (let char of chars) {
-                if (frequencyMap.has(char)) {
-                    frequencyMap.set(char, frequencyMap.get(char) + 1);
-                } else {
-                    frequencyMap.set(char, 1);
-                }
+    function getCharFrequency(text) {
+        let chars = text.replace(/\s/g, "").split("");
+        let frequencyMap = new Map();
+        for (let char of chars) {
+            if (frequencyMap.has(char)) {
+                frequencyMap.set(char, frequencyMap.get(char) + 1);
+            } else {
+                frequencyMap.set(char, 1);
             }
-            return frequencyMap;
         }
+        return frequencyMap;
+    }
 
 
     // Обработчик события клика на кнопку количества символов
-        charCountButton.addEventListener("click", function () {
-            let text = inputText.value;
-            let charFrequencyMap = getCharFrequency(text);
-            if (charFrequencyMap.size > 0) {
-                charsStatsContainer.style.display = "block"; // Показываем блок статистики знаков
-                charsStatsList.innerHTML = ""; // Очищаем предыдущий список
-                const messageLi = document.createElement("li");
-                messageLi.style.fontWeight = 'bold';
-                messageLi.textContent = "Количество символов :";
-                charsStatsList.appendChild(messageLi);
-                // Формируем элементы списка для каждой записи в карте частот
-                for (let [char, count] of charFrequencyMap.entries()) {
-                    let listItem = document.createElement("li");
-                    listItem.textContent = `${char} — ${count}`;
-                    charsStatsList.appendChild(listItem);
-                }
-            } else {
-                // Добавляем сообщение в список
-                charsStatsContainer.style.display = "block";
-                const messageLi = document.createElement("li");
-                messageLi.textContent = "Нет символов для анализа!";
-                charsStatsList.appendChild(messageLi);
+    charCountButton.addEventListener("click", function () {
+        let text = inputText.value;
+        let charFrequencyMap = getCharFrequency(text);
+        if (charFrequencyMap.size > 0) {
+            charsStatsContainer.style.display = "block"; // Показываем блок статистики знаков
+            charsStatsList.innerHTML = ""; // Очищаем предыдущий список
+            const messageLi = document.createElement("li");
+            messageLi.style.fontWeight = 'bold';
+            messageLi.textContent = "Количество символов :";
+            charsStatsList.appendChild(messageLi);
+            // Формируем элементы списка для каждой записи в карте частот
+            for (let [char, count] of charFrequencyMap.entries()) {
+                let listItem = document.createElement("li");
+                listItem.textContent = `${char} — ${count}`;
+                charsStatsList.appendChild(listItem);
             }
-        });
+        } else {
+            // Добавляем сообщение в список
+            charsStatsContainer.style.display = "block";
+            const messageLi = document.createElement("li");
+            messageLi.textContent = "Нет символов для анализа!";
+            charsStatsList.appendChild(messageLi);
+        }
+    });
 
 
 
